32da3fae9b83e52ff65d164128a978c2fe903918
Richard Banasiak <richard@banasiak.com>
2013-05-21 19:11:27 -0500

    Closed serial port at the end of SerialPortSinkSoftTouch so it can be opened again before the OS lock expires.

--- src/main/java/com/banasiak/ambilight/sink/SerialPortSinkSoftTouch.java
+++ src/main/java/com/banasiak/ambilight/sink/SerialPortSinkSoftTouch.java
@@ -7,51 +7,57 @@ import com.banasiak.ambilight.api.AmbilightException;
 
 public class SerialPortSinkSoftTouch {
 
-	public static boolean canTouchSerialPortSink() 
+	public static boolean canTouchSerialPortSink()
 	{
 		return canTouchSerialPortSink(null);
 	}
-	public static boolean canTouchSerialPortSink(String serialPort) 
+	public static boolean canTouchSerialPortSink(String serialPort)
 	{
 		final String fakeSerialPort = "faAkEserIal";
-		String useSerialPort = (serialPort != null) ? serialPort : fakeSerialPort;
+		final String useSerialPort = (serialPort != null) ? serialPort : fakeSerialPort;
 		boolean ret = false;
+		SerialPortSink obj = null;
 		try {
-			Class<?> c = Class.forName("com.banasiak.ambilight.sink.SerialPortSink");
-			Constructor cons = c.getConstructor(java.lang.String.class);
+			final Class<?> c = Class.forName("com.banasiak.ambilight.sink.SerialPortSink");
+			final Constructor cons = c.getConstructor(java.lang.String.class);
 			try
 			{
-				Object obj = cons.newInstance(useSerialPort);
+				obj = (SerialPortSink) cons.newInstance(useSerialPort);
 				ret = (obj != null);
 			}
-			catch (InstantiationException e) 
+			catch (final InstantiationException e)
 			{
 				if (useSerialPort.equals(fakeSerialPort))
 				{
 					// "probably ok" if fakeSerialPort was used? ?TODO?
 					ret = true;
 				}
-				else 
+				else
 				{
 					//overloaded meaning here : the library load worked, but your serial port didn't
 					// since we're trying to prevent "kill the JVM errors", we return true ?TODO?
+
 					ret = true;
 				}
-			} 
-			catch (IllegalAccessException e) {
+			}
+			catch (final IllegalAccessException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			} catch (InvocationTargetException e) {
+			} catch (final InvocationTargetException e) {
 				// If you do NOT have the library .so/.dll, you get
 				//   UnsatisifiedLinkError inside InvocationTargetException
 				ret = false;
 			}
-		} catch (ClassNotFoundException e) {
+		} catch (final ClassNotFoundException e) {
 			ret = false;
-		} catch (NoSuchMethodException e) {
+		} catch (final NoSuchMethodException e) {
 			throw new AmbilightException("Constructor not found");
-		} catch (SecurityException e) {
-			throw new AmbilightException("Constructor not found");			
+		} catch (final SecurityException e) {
+			throw new AmbilightException("Constructor not found");
+		}
+		// have to close the serial port manually, otherwise we can't open it again
+		if(obj != null){
+		    obj.closeSerialPort();
 		}
 		return ret;
 	}
